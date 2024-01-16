ff1ee8de7a9829a88d1fd3a03c936703a72b6955
Yasaswini-3004 <desuyasaswini@gmail.com>
2024-01-16 22:51:53 +0530

    Add or move React file to main branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+<<<<<<< HEAD
 import './App.css';
 import ChipInput from './Components/ChipInput';
+=======
+import React from 'react';
+import './App.css';
+import Select from './Components/ChipInput';
+
+>>>>>>> b58ac9c (Initial commit)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <ChipInput />
     </div>  
+=======
+      
+      <Select />
+    </div>
+>>>>>>> b58ac9c (Initial commit)
   );
 }
 
